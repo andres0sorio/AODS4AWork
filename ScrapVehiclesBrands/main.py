26d604a0c25e009a91607fa181c11ca2144bfe1f
Andres Osorio <osorio.af@gmail.com>
2021-07-10 22:08:13 -0500

    Added motorcycle list and processing

--- ScrapVehiclesBrands/main.py
+++ ScrapVehiclesBrands/main.py
@@ -3,13 +3,15 @@ from src.CarroYaScraper import CarroYaScraper
 
 if __name__ == '__main__':
 
-    step = 2
+    step = 3
     scraper = CarroYaScraper()
 
     if step == 1:
         scraper.scrape()
     elif step == 2:
         scraper.process()
+    elif step == 3:
+        scraper.process_motos()
     else:
         print("All done!")
 
--- ScrapVehiclesBrands/src/CarroYaScraper.py
+++ ScrapVehiclesBrands/src/CarroYaScraper.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import urllib
 import urllib.request
 import csv
+import json
 
 
 class CarroYaScraper:
@@ -10,8 +11,10 @@ class CarroYaScraper:
     """
     def __init__(self):
         self.carroya_url = 'https://www.carroya.com/marcas-carros/'
+        self.carroya_motos_url = 'https://www.carroya.com/marcas-motos/'
         self.results = []
         self.outfile = 'output/carroya_colombia_cars.html'
+        self.outfile_motos = 'output/carroya_colombia_motos.html'
 
     def scrape(self):
 
@@ -21,6 +24,14 @@ class CarroYaScraper:
 
         # Saving scraped HTML to .html file (for later processing)
         with open(self.outfile, 'w') as f_out:
+            f_out.write(site_content)
+
+        print(self.carroya_motos_url)
+        r = urllib.request.urlopen(self.carroya_motos_url)
+        site_content = r.read().decode('utf-8')
+
+        # Saving scraped HTML to .html file (for later processing)
+        with open(self.outfile_motos, 'w') as f_out:
             f_out.write(site_content)
 
     def process(self):
@@ -40,16 +51,54 @@ class CarroYaScraper:
                         brand_model.append(data[2])
                         self.results.append(brand_model)
 
-        self.export_to_CSV()
+        self.export_to_CSV("output/carros_colombia.csv")
+        self.export_to_JSON("output/carros_colombia.json")
 
-    def export_to_CSV(self):
+    def process_motos(self):
+
+        with open(self.outfile_motos) as f_in:
+            soup = BeautifulSoup(f_in, 'html.parser')
+            all_links = soup.find_all('a', href=True)
+            for a in all_links:
+                href = a["href"]
+                if "motos" in href:
+                    brand_model = []
+                    data = href.split('/')
+                    data = list(filter(lambda x: x != "", data))
+                    print(data)
+                    if len(data) == 3 and data[0] == "motos":
+                        brand_model.append(data[1])
+                        brand_model.append(data[2])
+                        self.results.append(brand_model)
+
+        self.export_to_CSV("output/motos_colombia.csv")
+        self.export_to_JSON("output/motos_colombia.json")
+
+    def export_to_CSV(self, outfile_name):
         """
 
         """
 
-        output_file = open("output/carros_colombia.csv", 'w', newline='')
+        output_file = open(outfile_name, 'w', newline='')
 
         with output_file:
             writer = csv.writer(output_file, delimiter=';')
             writer.writerow(["Marca", "Modelo"])
             writer.writerows(self.results)
+
+    def export_to_JSON(self, outfile_name):
+
+        vehicles = {}
+
+        for info in self.results:
+            brand = info[0]
+            vehicles[brand] = []
+
+        for info in self.results:
+            brand = info[0]
+            models = info[1]
+            vehicles[brand].append(models)
+
+        with open(outfile_name, 'w') as f_out:
+            json.dump(vehicles, f_out)
+
